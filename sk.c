77cbd4d86aa6798602c73447d3e255be3dc33a28
cgnneto <cgnneto@032d21f2-9d4a-0410-97f3-4d3649d09628>
2008-04-13 04:33:11 +0000

    Funcao de realoca para vetor de SK e funcao que carrega o indiceSK para RAM, quando ja existe um arquivo .sk .

--- sk.c
+++ sk.c
@@ -3,8 +3,47 @@
 
 IndSec * geraSk(TIndice *, FILE *, const int).
 
-IndSec * carregaSk(FILE *).
+/*Funcao que,caso exista o arquivo de SKs, carrega a parte que deve ficar na RAM e no no cabeca da AVAIL LIST da parte que fica no disco*/
+IndSec * carregaSk(FILE *arqSk){
+	/*Declaracao de variaveis*/
+	IndSec *sk;
+	int tamSk = 0;
+	
+	/*Inicializando o vetor de SKs*/
+	sk = (IndSec *)malloc(sizeof(IndSec)); 
+  sk->vetor = (Sk *) malloc(sizeof(Sk) * VETOR_MIN);
+  sk->alocado = VETOR_MIN;
+  sk->tamanho = 0;
+	
+	/*Leitura do no cabeca e do tamanho do SK que fica no disco*/
+	fscanf(arqSk, "%d", sk->head);
+	fscanf(arqSk, "%d", sk->tamDisco);
+
+	/*Posiciona o ponteiro do arquivo para o inicio da parte que deve ficar na RAM*/
+	fseek(arqSk, sek->tamDisco, SEEK_SET);
+
+	/*Enquanto nao chega ao final do arquivo, */
+	while (fscanf(arqSk, "%d", sk->vetor[tamSk].lenght) ){
+		fgets(sk->vetor[tamSk].key, sk->vetor[tamSk].lenght, arqSk);
+		tamSK++;
+		fscanf(arqSk, "%d", sk->vetor[tamSk].next);
+		sk = realocaIndSec(sk);
+	}
+	return sk;
+}.
 
 IndSec * criaSk(TIndice *, FILE *, const int).
 
 IndSec * insereSk(SkVetor *, char *).
+
+
+/* Realoca espaco para o vetor caso seja necessario. */
+IndSec * realocaIndSec(IndSec *sec) {
+
+  if (sec->tamanho == sec->alocado) {
+    sec->alocado *= 2;
+    sec->vetor = (Sk *) realloc(sec->vetor, sizeof(Sk) * sec->alocado);
+  }
+
+  return sec;
+}

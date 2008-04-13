9676a9bea1590e6b0cf50a68adb1fd47e6d17849
hnrqbaggio <hnrqbaggio@032d21f2-9d4a-0410-97f3-4d3649d09628>
2008-04-13 02:54:50 +0000

    Adicionando .c da biblioteca do indice secundario.

---
+++ sk.c
@@ -0,0 +1,10 @@
+#include <sk.h>
+
+
+IndSec * geraSk(TIndice *, FILE *, const int).
+
+IndSec * carregaSk(FILE *).
+
+IndSec * criaSk(TIndice *, FILE *, const int).
+
+IndSec * insereSk(SkVetor *, char *).

e5e203808b390013a9dbd5872cadbe98782ba19b
hnrqbaggio <hnrqbaggio@032d21f2-9d4a-0410-97f3-4d3649d09628>
2008-05-28 21:26:58 +0000

    Uma função Hash simples pra começar.

--- hash.h
+++ hash.h
@@ -1,4 +1,17 @@
 #ifndef HASH_H_
 #define HASH_H_
 
+#include <string.h>
+#define H 53
+
+int hashFunction(char * chave) {
+	int h = 0, i;
+	
+	for (i = 0; i < strlen(chave); ) {
+		h += (chave[i] * (++i)) % H
+	}
+	
+	return h % H;
+}
+
 #endif /*HASH_H_*/

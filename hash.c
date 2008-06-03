8bc9aa40e59e3b6d275429d842b7713ad31cd92e
hnrqbaggio <hnrqbaggio@032d21f2-9d4a-0410-97f3-4d3649d09628>
2008-06-03 20:28:47 +0000

    Função de hash do descritor, que usa o numero de bits ligados.

--- hash.c
+++ hash.c
@@ -9,3 +9,14 @@ int hashFunction(char * chave) {
 	
 	return h % H;
 }
+
+int hashDescritor(char descritor) {
+	int n = 0, m;
+	
+	/* Faz & com potencias de 2 para ver quais bits do descritor estão ligados. */
+	for (m = 1; m < 256; m*=2) {
+		if (descritor & m) n++;
+	}
+	
+	return n;
+}

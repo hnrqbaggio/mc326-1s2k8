c5e9684059081aa163f8c439bd883086c876a50f
hnrqbaggio <hnrqbaggio@032d21f2-9d4a-0410-97f3-4d3649d09628>
2008-06-07 21:40:32 +0000

    Leitura.h atualizado.

--- leitura.h
+++ leitura.h
@@ -2,7 +2,6 @@
 #define _LEITURA_H
 
 #include <stdio.h>
-#include <stdio_ext.h>
 #include <string.h>
 #include "catalogo.h"
 #include "sk.h"
@@ -13,7 +12,7 @@
  * Recebe o indice primario para verificar se a obra ja existe e atualizar este.
  * Retorna a obra a ser inserida na base.
  */
-TObra * leObra(IndicePrim *, TObra *);
+TObra * leObra(IndPrim *, TObra *);
 
 /*_________Funcoes de leitura das entradas do usuario_________*/
 /** 
@@ -22,7 +21,7 @@ TObra * leObra(IndicePrim *, TObra *);
  * entradas invalidas. Ao final de uma entrada do usuario, ele grava
  * os dados na base e atualiza o vetor de busca na memoria.
  */
-void insereObra(FILE *, IndicePrim *);
+void insereObra(FILE *, IndPrim *);
 
 /**
  * Esta funcao realiza a leitura dos campos do tipo texto: Titulo,
@@ -53,5 +52,26 @@ void leImagem(char *, int, char *);
  * tamanho dos campos fixos quando forem salvos na base.
  */
 void preencher(char *, int);
+
+/** 
+ * Mensagem de erro enviada ao usuario caso uma entrada seja grande
+ * demais para o campo da obra, ou esteja fora do padrao
+ * estabelecido. 
+ */
+#define MensagemErro() (printf("Entrada invalida. Digite novamente.\n"))
+
+/** 
+ * Pseudo-funcao usada para limpar o buffer do teclado e descartar o
+ * excesso de dados digitado pelo usuario numa entrada.  
+ * Nota: A funcao __fpurge eh incompativel com Windows (Testado no LMS).
+ */
+#ifdef IS_WINDOWS
+	#define LimpaBuffer() (fflush(stdin))
+#else
+	#include <stdio_ext.h>
+	#define LimpaBuffer() (__fpurge(stdin))
+#endif
+
+
 
 #endif

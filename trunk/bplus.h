a5a0b695a642d3133be125bed79fa798af728b4c
hnrqbaggio <hnrqbaggio@032d21f2-9d4a-0410-97f3-4d3649d09628>
2008-06-21 21:33:40 +0000

    mais tp5

--- trunk/bplus.h
+++ trunk/bplus.h
@@ -1,95 +1,36 @@
 #ifndef BPLUS_H_
 #define BPLUS_H_
-
-/* Operacoes na Arvore B+
- * Insert - Inserir uma chave numa folha
- * Remove - Remover uma chave de uma folha
- * Search - Encontrar a folha que contem uma chave
- * 
- * Operacoes sobre os nos
- * Split - Quebra um noh em dois, quando falta espaco
- * Merge - Junta dois nos
- * 
- * Operacoes sobre folhas
- * Insert - Inserir a chave no arquivo de indice
- * Remove
- * MakeLeaf - Cria uma nova folha, alocando espaco para ela
- * 
- * */
-
-/* == Insercao em um noh ==
- * 
- * Procura pela CHAVE - busca sequencial ou naum pelo vetor de keys
- *
- * Se o nivel eh 1
- * 	insere na folha filha
- * Senao
- * 	insere no noh filho (recursao)
- * 
- * Se houve overflow do filho
- * 	Insere uma nova key que eh igual a CHAVE, antes da key do filho que teve overflow.
- * 		(isso implica em mover os ponteiros também ¬¬)
- * 	Se o filho for um noh
- *  	Cria um novo filho, e transfere a primeira metade do do filho 
- *   	com over pro novo. Associa esse novo filho a nova chave inserida.
- * 	Se o filho for uma folha
- * 		cria uma nova folha, e copia os primeiros registros pra um novo indice, 
- * 		associado a nova folha, entao salva ambos os indices.
- * 
- * Se inseriu normalmente, retorne 1
- * Se a chave jah existia, retorne 0
- * Se deu overflow do noh (nao do filho), retorne -1
- * 
- * IMPORTANTE - OVERFLOW DO ROOT
- * Cria um novo ROOT com a maior chave do atual
- * Cria um novo noh e coloca o atual ROOT e o novo noh 
- * 	como filhos do novo ROOT.
- * Salva os dois nos e tambem o novo ROOT.
- * Retorna -1 (isso faz o main recarregar o root, que vai tah atualizado)
- */
- 
- /* == Insercao numa folha ==
-  * Recebe uma string, a CHAVE
-  * Carrega o indice da memoria pro arquivo
-  * Insere normalmente no indice
-  * Salva o indice no arquivo
-  * 
-  * Se deu overflow do indice, retorne -1
-  * Se inseriu normalmente, retorne 1
-  * Se a chave jah existia, retorne 0
-  */
-
 #include <stdio.h>
-#include <string.h>
-#include "tipos.h"
-#include "index.h"
 
-BTNode * makeNode(int);
-BTLeaf * makeLeaf(int);
+typedef struct {
+	int numChaves; /**< Numero de chaves no noh. Usado para controlar operacoes da arvore. */
+ 	int chaves[B_ORDER-1] /**< vetor de strings que sao os delimitadores das chaves. */
+	int filhos[B_ORDER]; /**< Vetor de ponteiros para os filhos do noh. */
+	int level; /**< Nivel do noh - sua altura. A raiz tem level = altura - 1. */
+	int left;
+	int right;
+} BTNode;
 
-int insert(char *key, BTNode *node, Index *indice);
-int search(char *key, BTNode *node, Index *indice);
-int insertInLeaf(char *key, BTLeaf *leaf);
+/* Funcoes */
+/* Insercao. retorno: 1:ok, 0:repetido, -1:overflow do node */
+int insert(int chave, int nodeId);
 
-void merge(BTNode *);
-void split(BTNode *, BTNode *);
+int bsearch();
+int remove();
 
-/* Faz a leitura de uma pagina para um noh. */
-void loadNode(BTNode *node, int id, char * campo);
+/* Quebra um noh, retorna o ID do novo. */
+int split(BTNode *cheio, BTNode *novo);
 
-/* Carrega uma folha do disco, a partir do id. */
-BTLeaf * loadLeaf(int id, char *tipoCampo);
+int merge();
 
-/* Cria um novo noh, e uma pagina associada a ele, com um novo ID. */
-int createNewNode(BTNode **novoFilho, int nivel, char *campo);
+/* Aloca memoria. */
+BTNode makeNode(void);
 
-/* Grava os dados de um noh na sua pagina. */
-void writeNode(BTNode *filho, int nodeId, char *tipoCampo);
-void writeLeaf(BTLeaf *folha, char * tipoCampo);
+/* I/O */
+void readNode(BTNode *node, int nodeId);
+void writeNode(BTNode *node, int nodeId);
+void leRegistro(); /* Pegar em catalogo.c */
 
-/* Obtem o ID para um novo noh ou folha. */
-int getId(char *);
-
-void rootOverFlow(Index *);
+int getId();
 
 #endif /*BPLUS_H_*/

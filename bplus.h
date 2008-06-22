da71bc3bcc17a141251befe0c216bb4540db2172
cgnneto <cgnneto@032d21f2-9d4a-0410-97f3-4d3649d09628>
2008-06-22 22:09:49 +0000

    Mudancao na insert e .h. Criacao de funcoes auxiliares.Algumas ainda com pseudo codigo.

--- bplus.h
+++ bplus.h
@@ -1,43 +1,129 @@
 #ifndef BPLUS_H_
 #define BPLUS_H_
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * Definicoes para facilitar a vizualizacao do codigo da arvore B+
+*/
 #define TRUE 1
 #define FALSE 0
 #define OVERFLOW -1
-
 #define RIGHT 1
 #define LEFT 2
 
+/**
+ * Ordem da arvore b+.
+*/
+#define B_ORDER 2
+
+/**
+ * Tamanho maximo do nome do arquivo.
+*/
+#define TAM_NOME_ARQ 60
+
+/**
+ * Extencao do arquivo reservado para gravar o id do ultimo arquivo gravado.
+*/
+#define ARQUIVO_ID "id.bplus"
+
+/**
+ * Extencao do arquivo de no da arvore.
+*/
+#define EXTENSAO_NODE ".bplus"
+
 typedef struct {
 	int numChaves; /**< Numero de chaves no noh. Usado para controlar operacoes da arvore. */
- 	int chaves[B_ORDER-1] /**< vetor de strings que sao os delimitadores das chaves. */
-	int filhos[B_ORDER]; /**< Vetor de ponteiros para os filhos do noh. */
+ 	int chaves[B_ORDER-1]; /**< vetor de inteiros que sao os delimitadores das chaves. */
+  int filhos[B_ORDER]; /**< Vetor de ponteiros para os filhos do noh. */
 	int leaf; /**< Nivel do noh - sua altura. A raiz tem level = altura - 1. */
-	int left;
-	int right;
+	int left; /**< Apontador para esquerda. */
+	int right;/**< Apontador para direita.*/
+  int id; /**<Id do arquivo para ser gravado em disco. */
 } BTNode;
 
+typedef struct {
+  int key; /**<Inteiro que e a pk do indice.*/
+  int pointer; /**<Inteiro usado como o nrr do indice primario. */
+} pk;
+
 /* Funcoes */
-/* Insercao. retorno: 1:ok, 0:repetido, -1:overflow do node */
-int insert(int chave, int nodeId);
 
-int bsearch();
+/**
+ * @brief Funcao de insercao.
+ * 
+ * Funcao que busca recursivamente o local a ser inserido.
+ * 
+ * @param key A chave a ser inserida.
+ * @param nodeId O no a ser aberto para a insercao.
+ * @return  1:ok, 0:repetido, -1:overflow do node 
+*/
+int insert(pk *key, int nodeId);
+
+/**
+ * @brief Aloca memoria para no da arvore.
+ * 
+ * @return O no alocado em memoria, inicializado com o nivel.
+*/
+BTNode * makeNode();
+
+/* I/O */
+
+/**
+ * @brief Le um arquivo da arvore, no ou folha.
+ * 
+ * @param node O no em memoria onde sera armazenado.
+ * @param nodeId O id do arquivo a ser carregado.
+*/
+void readNode(BTNode *node, int nodeId);
+
+/**
+ * @brief Grava um no da arvore para o disco.
+ * 
+ * @param node O no a ser gravado.
+*/
+void writeNode(BTNode *node);
+
+/**
+ * @brief Le do arquivo o inteiro que sera o nome do arquivo a ser gravado na arvore.
+ * 
+ * @return O numero do novo arquivo a ser gravado.
+*/
+int getId();
+
+/**
+ * @brief Carrega um arquivo que possui overflow, e faz split. 
+ * 
+ * @param nodeId O id do arquivo a ser carregado.
+ * @return Pk contendo o middle a ser inserido no pai.*/
+pk split(int nodeId);
+
+/**
+ * @brief Funcao que faz rotacao no no/folha da arvore.
+ * 
+ * @param id O id do arquivo a ser rotacionado.
+ * @param idIrmao O id do arquivo para onde sera transferido os dados de id.
+ * @param tipo LEFT/RIGHT Define se a rotacao e para esquerda ou direita. 
+ * @return 1 caso rotacao ocorra com sucesso, 0 caso nao ocorra a rotacao.
+*/
+int rotation(int id, int idIrmao, const int tipo);
+
+
+void leRegistro(); /* Pegar em catalogo.c */
+
+
+/**
+ * @brief Funcao recursiva de busca na arvore.
+ * 
+ * @param key A chave a ser buscada.
+ * @param BTNode O no atual a ser buscado.
+ * return O ponteiro do arquivo de indice a ser aberto.
+*/
+int search(char *key, BTNode *node);
+
 int remove();
-
-/* Quebra um noh, retorna o ID do novo. */
-int split(BTNode *cheio, BTNode *novo);
 
 int merge();
 
-/* Aloca memoria. */
-BTNode makeNode(void);
-
-/* I/O */
-void readNode(BTNode *node, int nodeId);
-void writeNode(BTNode *node, int nodeId);
-void leRegistro(); /* Pegar em catalogo.c */
-
-int getId();
 
 #endif /*BPLUS_H_*/
